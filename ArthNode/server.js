eb566ad22c7e73579ae887a982f5a7e422278e74
Arthur Gorr <art.gorr@gmail.com>
2014-12-31 08:02:21 -0600

    Install vash view engine and initial view

--- ArthNode/server.js
+++ ArthNode/server.js
@@ -1,6 +1,14 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var http = require("http");
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+    res.render("index", { title: "Project ArthNode" });
+});
+
+app.set("view engine", "vash"); // vash view engine
+
+var server = http.createServer(app);
+
+
+server.listen(3000);
