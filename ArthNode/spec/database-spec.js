834fcbed49012521b91d98e1b7be8141dbe6fad3
Arthur Gorr <art.gorr@gmail.com>
2015-01-03 21:48:55 -0600

    Introduced a test but could not get it to work

---
+++ ArthNode/spec/database-spec.js
@@ -0,0 +1,13 @@
+﻿var database = require("../data/database");
+
+describe("database.getDB", function() {
+    xit("should initialize DB object with collections", function() {
+        database.getDb(function (err, db) {
+            // could not get this to work
+            console.log("hello jasmine");
+            expect(err).toBeNull();
+            expect(db.db).toBeDefined();
+            expect(db.workouts).toBeDefined();
+        });
+    });
+});
