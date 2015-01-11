400a4b37c04ee28fe782e7d1d210f8bcc3734bd1
Arthur Gorr <art.gorr@gmail.com>
2015-01-10 22:35:34 -0600

    Add specification for arthNode controller

---
+++ ArthNode/spec/arthNodeController-spec.js
@@ -0,0 +1,19 @@
+﻿describe("ArthNode controller", function () {
+    var arthNodeController;
+    
+    beforeEach(function () {
+        module('arthNode');
+    });
+    
+    beforeEach(inject(function ($controller) {
+        arthNodeController = $controller('arthNodeCtrl');
+    }));
+    
+    it("should be defined", function () {
+        expect(arthNodeController).not.toBeNull();
+    });
+    
+    it("should contain a reference to the unit of measure service", function () {
+        expect(arthNodeController.arthNodeService).not.toBeNull();
+    });
+});
