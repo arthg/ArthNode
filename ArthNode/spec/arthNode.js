a20b9d950549f96d8fa842e9355dab85b6b518a1
Arthur Gorr <art.gorr@gmail.com>
2015-01-10 21:09:26 -0600

    arthNode spec (with fake test) more initial setup stuff

---
+++ ArthNode/spec/arthNode.js
@@ -0,0 +1,15 @@
+﻿describe("Formula for BMI", function () {
+    
+    describe("Given a 5 ft 10 person who weighs 300 pounds", function () {
+        
+        it("should compute a BMI of 43", function () {
+            var actual = formulaBmi({
+                height: 70,
+                weight: 300
+            });
+            expect(actual).toBeCloseTo(43);
+        });
+
+    });
+    
+});
